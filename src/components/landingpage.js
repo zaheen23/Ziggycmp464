823b4abd3858545e4b204a0384d2751da4b01b50
Zaheen Shahriyar <zaheenshahriyar@Zaheens-MacBook-Air.local>
2020-12-18 23:40:56 -0500

    linkin

---
+++ src/components/landingpage.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
+
+
+class Landing extends Component {
+  render() {
+    return(
+      <div style={{width: '100%', margin: 'auto'}}>
+        <Grid className="landing-grid">
+          <Cell col={11}>
+            
+            <p> blank</p>
+
+            <div className="banner-text">
+              <h1>Zaheen Shahriyar</h1>
+              <h1>Rising Web Developer</h1>
+
+            <hr/>
+
+          <p>HTML/CSS | JavaScript | React </p>
+
+        <div className="social-links">
+
+
+          {/* Github */}
+          <a href="https://github.com/zaheen23" rel="noopener noreferrer" target="_blank">
+            <i className="fa fa-github-square" aria-hidden="true" />
+          </a>
+
+          {/* Youtube */}
+          <a href="https://www.youtube.com/watch?v=42U-oJl1k_s&ab_channel=ZaheenShahriyar" rel="noopener noreferrer" target="_blank">
+            <i className="fa fa-youtube-square" aria-hidden="true" />
+          </a>
+
+        </div>
+            </div>
+          </Cell>
+        </Grid>
+      </div>
+    )
+  }
+}
+
+export default Landing;
